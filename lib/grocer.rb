0291fb72d270f9e12e1c42f18e630794f234e2e5
Ori Markowitz <ori.markowitz@gmail.com>
2020-11-15 17:21:40 +0000

    Automatically backed up by Learn

--- lib/grocer.rb
+++ lib/grocer.rb
@@ -2,7 +2,7 @@ def find_item_by_name_in_collection(name, collection)
   result_hash = nil
   collection.each do |product|
     if product[:item] == name
-      result_hash = item
+      result_hash = product
     end
   end
   result_hash
